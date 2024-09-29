ab1a7d457787e2d7def7941d89fe3ef0b230da24
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-09-29 23:46:27 +0200

    Backend start

--- 2-portfolio/backend/src/index.ts
+++ 2-portfolio/backend/src/index.ts
@@ -1,10 +1,11 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { app } from "./app"
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json('Hello Hono!')
 })
 
 const port = 3999
--- 2-portfolio/frontend/src/components/Header.tsx
+++ 2-portfolio/frontend/src/components/Header.tsx
@@ -10,11 +10,11 @@ export default function Header({user, degree, educationPoints}: UserProps){
     return(
         <header className="header">
             <div className="static-elements">
-            <img src="https://placehold.co/50x50" alt="Placeholder" />
+            <img src="https://placehold.co/500x500" alt="Placeholder" />
             <h1>Portfolio</h1>
             <h2 id="name">{user}</h2>       
             </div>
-            <p> <strong> {educationPoints} </strong> {degree} </p> 
+            <p>{degree} | Credits: {educationPoints} </p> 
         </header>
     )
 }
